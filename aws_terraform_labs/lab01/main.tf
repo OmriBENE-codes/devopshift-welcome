c193fc4c455fe732dec37ef837a4b036ed091a1a
Your Name <you@example.com>
2025-07-09 16:05:33 +0000

    lab01 2cnd

--- aws_terraform_labs/lab01/main.tf
+++ aws_terraform_labs/lab01/main.tf
@@ -36,3 +36,16 @@ resource "time_sleep" "wait_for_ip" {
   depends_on = [aws_instance.vm]
 }
 
+resource "null_resource" "check_public_ip" {
+  provisioner "local-exec" {
+    command = <<EOT
+      if [ -z "${aws_instance.vm.public_ip}" ]; then
+        echo "ERROR: Public IP address was not assigned." >&2
+        exit 1
+      fi
+    EOT
+  }
+
+  depends_on = [aws_instance.vm]
+}
+
--- aws_terraform_labs/lab01/outputs.tf
+++ aws_terraform_labs/lab01/outputs.tf
@@ -3,3 +3,8 @@ output "vm_public_ip" {
   description = "Public IP address of the VM"
   depends_on  = [time_sleep.wait_for_ip]
 }
+output "vm_public_ip_check" {
+  value      = aws_instance.vm.public_ip
+  depends_on = [null_resource.check_public_ip]
+  description = "Public IP address of the VM"
+}
